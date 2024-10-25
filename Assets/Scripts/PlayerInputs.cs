ca817cdac0cbf63d738742d7374668c421a1c597
Furkansnr <furkansnr00@gmail.com>
2024-10-25 17:51:48 +0300

    Initial commit

---
+++ Assets/Scripts/PlayerInputs.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerInputs : MonoBehaviour
+{
+    public Vector2 moveInput;
+    
+    public void OnMove(InputValue value)
+    {
+        moveInput = value.Get<Vector2>();
+    }
+    
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] PlayerInputs _inputs;
+    [SerializeField] private Rigidbody2D _rigidbody2D;
+    [SerializeField] private float moveSpeed = 5f;  
+    
+    private void FixedUpdate()
+    {
+        _rigidbody2D.velocity = new Vector2(_inputs.moveInput.x * moveSpeed,_inputs.moveInput.y * moveSpeed);
+    }
+}
